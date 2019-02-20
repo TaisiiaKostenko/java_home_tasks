f3cac75fd498b889b148eee563bab1662819097d
TayisiyaKostenko <47828374+TayisiyaKostenko@users.noreply.github.com>
2019-02-20 21:01:32 +0200

    Add files via upload

---
+++ task_1.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+public class task_1 {
+
+    public static void main(String[] args) {
+            System.out.println("****  ****    *   *      *");
+            System.out.println("*     *  *   * *  * *  * *");
+            System.out.println("**    ****  *  *  *  *   *");
+            System.out.println("*     *    *****  *      *");
+            System.out.println("****  *   *    *  *      *");
+    }
+}
