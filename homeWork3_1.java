3876d527e9f4ddc782650b771facf252c574912e
TaisiiaKostenko <47828374+TaisiiaKostenko@users.noreply.github.com>
2019-03-04 15:33:14 +0200

    Update homeWork3_1.java

--- homeWork3_1.java
+++ homeWork3_1.java
@@ -6,13 +6,13 @@ public class homeWork3_1 {
 
         for (int i = 0; i < count; i++) {
             for (int k = 0; k < i + 1; k++) {
-                System.out.print("*");
+                System.out.print(" *");
             }
             System.out.println();
         }
         for (int i =0; i < count; i++) {
             for (int k = 10; k > i; k--) {
-                System.out.print("*");
+                System.out.print(" *");
             }
             System.out.println();
         }
