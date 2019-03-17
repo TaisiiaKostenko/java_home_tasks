40b720db3ff52cd1d073756e8e799fb838b46764
TaisiiaKostenko <47828374+TaisiiaKostenko@users.noreply.github.com>
2019-03-17 14:08:16 +0200

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,62 @@
+package Homework6;
+
+public class Book {
+    private int ID;
+    private String title;
+    private String author;
+    private String publishingOffice;
+    private int yearOfPublishing;
+    private int numberOfPages;
+    private double price;
+
+    public Book (int ID, String title, String author, String publishingOffice,
+                 int yearOfPublishing, int numberOfPages, double price){
+
+        this.ID = ID;
+        this.title = title;
+        this.author = author;
+        this.publishingOffice = publishingOffice;
+        this.yearOfPublishing = yearOfPublishing;
+        this.numberOfPages = numberOfPages;
+        this.price = price;
+        }
+
+    public Book () {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublishingOffice() {
+        return publishingOffice;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public double getPrice() {
+        return price;
+
+    }
+
+    public void view(){
+        System.out.println(" ID " + ID + " Title " + title + " Author " + author + " Publishing office " + publishingOffice
+                + " Year of publishing " + yearOfPublishing + " Number of pages " + numberOfPages + " Price " + price);
+
+    }
+
+}
